d15b3e46cd51f6dd10a6b4ae2328ac986c4f0262
JhonFXA <jonathanbispo085@gmail.com>
2024-10-05 23:27:01 -0300

    initial commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -21,7 +21,7 @@ public class ContaTerminal {
         System.out.println("Digite o saldo do cliente");
         saldo = scanner.nextFloat();
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
         
         scanner.close();
     }
